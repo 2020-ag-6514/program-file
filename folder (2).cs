7d33c4f14d3bbee053dd5755f01a5b81f1588aeb
Dhanak <ifzayousaf2@gmail.com>
2021-11-28 07:31:13 -0800

    first commit

---
+++ folder (2).cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace OOPExamPle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =3,b =4;
+            a=++a;
+            b=b++;
+            //(a)
+            Console.WriteLine("a");
+            Console.WriteLine(a*b);
+            //(b)
+            //If we replace ++a =b++ by int al =3,bl =4;
+            b =++b;
+            a =++a;
+            Console.WriteLine("b");
+            Console.WriteLine(a*b);
+
+            //(c)
+            Console.WriteLine("c");
+            a=a+1;
+            b=b+1;
+            Console.WriteLine(a*b);
+            }
+
+    }
+}
